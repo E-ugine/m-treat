ca327cbfd1635cf159ff4602cb023d5a0a61c718
EugineAgolla <agollaeugine@gmail.com>
2025-01-02 18:55:20 +0300

    creates and registers the patient model

--- mtreat/patients/models.py
+++ mtreat/patients/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Patient(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    password = models.CharField(max_length=255)  
+
+    def __str__(self):
+        return self.name
