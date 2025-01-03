8451c81eb2f905347e6a45316863a8c0c56be29f
EugineAgolla <agollaeugine@gmail.com>
2025-01-03 05:03:53 +0300

    creates vite react app and configures tailwind css

---
+++ mtreat/frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+
+export default function App() {
+  return (
+    <div>
+      <SignUp />
+      <Login />
+    </div>
+  );
+}
